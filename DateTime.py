8660a19c8851536c3ec8f5d3eca1340d672eeb8b
CoderAnonymos <adepojumayomikun1@gmail.com>
2024-11-21 18:51:56 +0000

    First Commit

---
+++ DateTime.py
@@ -0,0 +1,7 @@
+import datetime, calendar
+current_time = datetime.datetime.now()
+#datetime_object = datetime.datetime.now()
+print("The time now at GMT is: ", end="-")
+print(current_time)
+
+print("\n", calendar.calendar(2024))
